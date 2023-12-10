536b6aea225da5cde5f2454ee19ccf2b40e44804
elber <elber3910@gmail.com>
2023-12-10 08:09:27 -0300

    Mario Bros

---
+++ script.js
@@ -0,0 +1,10 @@
+let formulario = document.querySelector(".formulario");
+let mascara = document.querySelector(".mascara-form");
+
+function cliqueinobotao() {
+  formulario.style.left = "900px";
+  mascara.style.visibility = "visible";
+}
+function sumirformulario() {
+  formulario.style.left = -"-560px";
+}
